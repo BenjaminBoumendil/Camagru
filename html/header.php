a7605be43da1e7bd62332062299a4e825b734d4d
Benjamin BOUMENDIL <bboumend@e1r13p6.42.fr>
2017-02-08 16:22:55 +0100

    ajax on logout

--- html/header.php
+++ html/header.php
@@ -2,10 +2,18 @@
 <html>
 
   <header style="height:50px;width:100%;background-color:black;">
-    <p style="color:white;"> Welcome <?php $_SESSION['username'] ?></p>
-    <a href="/?logout">
-      <input style="float:right;" type="submit" name="action" value="Logout" />
-    </a>
+    <p style="color:white;"> Welcome</p>
+    <input onclick="logout()" style="float:right;" type="submit" name="action" value="Logout" />
   </header>
+
+  <script>
+    function logout() {
+        console.log("logout js");
+        xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "/?logout", true);
+        xhttp.send();
+        window.location.href = "/";
+    }
+  </script>
 
 </html>
